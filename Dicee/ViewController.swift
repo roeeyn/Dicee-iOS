ed289ef3f22296f20001d31670904a0a835f55a1
Rodrigo M <rodrigo.medina.neri@gmail.com>
2019-06-03 21:32:22 -0500

    Finished the roll dice ios app

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     var randomDiceIndex1:Int = 0
     var randomDiceIndex2:Int = 0
     
+    let diceNames = [
+        "dice1",
+        "dice2",
+        "dice3",
+        "dice4",
+        "dice5",
+        "dice6",
+    ];
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -25,7 +34,10 @@ class ViewController: UIViewController {
     @IBAction func rollButtonPressed(_ sender: Any) {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
-        print(randomDiceIndex1, randomDiceIndex2)
+        
+        diceImageView1.image = UIImage(named: diceNames[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceNames[randomDiceIndex2])
+        
     }
 }
 
